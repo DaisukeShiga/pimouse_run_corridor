81477d35b1f57e806ef17835ad74f1e4a6d93955
Daisuke Shiga <Daisuke.Shiga@sony.com>
2018-02-09 11:33:15 +0000

    test_wall_stop_accel 3

--- test/travis_test_wall_stop_accel.py
+++ test/travis_test_wall_stop_accel.py
@@ -25,7 +25,7 @@ class WallStopAccelTest(unittest.TestCase):
         self.assertTrue(0 < left == right < 1000, "cantot move agine")
         
         time.sleep(5.0)
-        left, right = self.set_and_get(400, 0, 0, 99) #total: 499
+        left, right = self.set_and_get(40, 0, 0, 9) #total: 49
         self.assertTrue(2000 < left == right, "cantot accerelate")
 
         left, right = self.set_and_get(15, 0, 20, 15) #total: 50
